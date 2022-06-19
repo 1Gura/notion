596f7b29a886564b3f2437110552e942b4411a8a
i.gura <gura.ilya2011@yandex.ru>
2022-06-19 18:22:42 +0300

    добавлена короткая модель

---
+++ src/app/components/root/shared/model/page-note-short.model.ts
@@ -0,0 +1,5 @@
+import { BaseModel } from '../../../../shared/model/base.model';
+
+export class PageNoteShortModel extends BaseModel {
+  public title: string = '';
+}
--- src/app/components/root/shared/model/page-note.model.ts
+++ src/app/components/root/shared/model/page-note.model.ts
@@ -1,8 +1,7 @@
-import { BaseModel } from '../../../../shared/model/base.model';
 import { ContentBaseModel } from './content-base.model';
+import { PageNoteShortModel } from './page-note-short.model';
 
-export class PageNoteModel extends BaseModel {
-  public title: string = '';
+export class PageNoteModel extends PageNoteShortModel {
   public content: ContentBaseModel[] = [];
   public userId: string = '';
 }
